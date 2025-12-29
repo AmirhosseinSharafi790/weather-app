bcd4dfbc659da2fd0c2ca689be62ae5c7427e7eb
amirhossein <theamir790@gmail.com>
2025-12-29 22:31:50 +0330

    add forecast data in prop at forecast items component

--- components/Forecast/Forecast.tsx
+++ components/Forecast/Forecast.tsx
@@ -1,22 +1,26 @@
 import { useTranslation } from "react-i18next";
 import ForecastItems from "./ForecastItems";
 
-export default function Forecast() {
-  const { t } = useTranslation()
+interface Props {
+  daily: any;
+}
+
+export default function Forecast({ daily }: Props) {
+  const { t } = useTranslation();
+
   return (
-    <div className="flex flex-col gap-y-2 items-start justify-center">
-      <div className="title">
-        {t("7_DayForecast")}
-      </div>
-      <ForecastItems />
-      <ForecastItems />
-      <ForecastItems />
-      <ForecastItems />
-      <ForecastItems />
-      <ForecastItems />
-      <ForecastItems />
-      <ForecastItems />
-      <ForecastItems />
+    <div className="flex flex-col gap-y-2">
+      <div>{t("7_DayForecast")}</div>
+
+      {daily.time.map((day: string, index: number) => (
+        <ForecastItems
+          key={day}
+          day={day}
+          min={daily.temperature_2m_min[index]}
+          max={daily.temperature_2m_max[index]}
+          code={daily.weather_code[index]}
+        />
+      ))}
     </div>
   );
 }
